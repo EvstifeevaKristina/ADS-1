fd1c2170a7934a57101277f32e4f42161352b3f6
EvstifeevaKristina <94647555+EvstifeevaKristina@users.noreply.github.com>
2022-03-05 23:22:06 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,46 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+ 
+if (value <= 1)
+    return false;
+ for (int i  = 2; i <= (value/2); i++) {
+   if ((value % i) == 0)
+    retutn false;
+ }
+  return true;
+  
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+
+ int count = 0;
+ uint64_t prime = 1;
+ while ( count != n ) {
+   prime++;
+   if (checkPrime(prime)) {
+     count++;
+    }
+  }
+  retutn prime;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+
+value++;
+ while (checkPrime(value) != true) {
+   value++;
+ }
+ return value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+
+uint64_t sum = 0;
+  for(int i = 2; i < hbound; i++) {
+    if (checkPrime(i)) {
+      sum += i;
+    }
+  }
+  return sum;
 }
